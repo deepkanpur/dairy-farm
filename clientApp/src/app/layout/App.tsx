adda67ce46563be4f1fa9f4e91d1d04c607017cc
Deep Singh Chauhan <Deep_Chauhan@Dell.com>
2024-04-24 16:22:35 -0500

    end of section 10 Error Handling

--- clientApp/src/app/layout/App.tsx
+++ clientApp/src/app/layout/App.tsx
@@ -3,17 +3,18 @@ import Navbar from "./navbar";
 import { observer } from "mobx-react-lite";
 import { Outlet, useLocation } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const location = useLocation();
 
   return (
     <>
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored'  />
       {location.pathname === "/" ? (
         <HomePage />
       ) : (
         <>
-          {" "}
           <Navbar />
           <Container style={{ marginTop: "5em" }}>
             <Outlet />
