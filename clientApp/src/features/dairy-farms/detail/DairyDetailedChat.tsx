a5abd2160f89178d7ad222c81bafce0b41aca930
Deep Singh Chauhan <Deep_Chauhan@Dell.com>
2024-05-18 01:24:36 -0500

    login to add comment

--- clientApp/src/features/dairy-farms/detail/DairyDetailedChat.tsx
+++ clientApp/src/features/dairy-farms/detail/DairyDetailedChat.tsx
@@ -1,18 +1,23 @@
 import { observer } from "mobx-react-lite";
-import { Segment, Header, Comment, Button } from "semantic-ui-react";
+import { Segment, Header, Comment, Button, Label } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Formik, Form } from "formik";
 import MyTextArea from "../../../app/common/form/MyTextArea";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 import { formatDistanceToNow } from "date-fns";
+import LoginForm from "../../users/LoginForm";
 
 interface Props {
   dairyFarmId: string;
 }
 export default observer(function DairyDetailedChat({ dairyFarmId }: Props) {
-  const { commentStore } = useStore();
+  const {
+    commentStore,
+    userStore: { isLoggedIn },
+    modalStore,
+  } = useStore();
 
   useEffect(() => {
     if (dairyFarmId) commentStore.createHubConnection(dairyFarmId);
@@ -33,16 +38,23 @@ export default observer(function DairyDetailedChat({ dairyFarmId }: Props) {
         <Header>Feedback about this Dairy</Header>
       </Segment>
       <Segment attached clearing>
-      <Formik 
-            onSubmit={(values, {resetForm}) => commentStore.addComments(values).then(() => resetForm())}
-            initialValues={{body: ''}}
+        {isLoggedIn && (
+          <Formik
+            onSubmit={(values, { resetForm }) =>
+              commentStore.addComments(values).then(() => resetForm())
+            }
+            initialValues={{ body: "" }}
             validationSchema={Yup.object({
-              body: Yup.string().required('Comment is required')
+              body: Yup.string().required("Comment is required"),
             })}
           >
-            {({isSubmitting, isValid, dirty}) => (
+            {({ isSubmitting, isValid, dirty }) => (
               <Form className="ui form">
-                <MyTextArea placeholder="Enter your comment..." name='body' rows={2} />
+                <MyTextArea
+                  placeholder="Enter your comment..."
+                  name="body"
+                  rows={2}
+                />
                 <Button
                   disabled={isSubmitting || !isValid || !dirty}
                   loading={isSubmitting}
@@ -56,21 +68,41 @@ export default observer(function DairyDetailedChat({ dairyFarmId }: Props) {
               </Form>
             )}
           </Formik>
+        )}
+        {!isLoggedIn && (
+          <>
+            <Label content="Please login to add comments" />
+            <Button
+              onClick={() => modalStore.openModal(<LoginForm />)}
+              positive
+              floated="right"
+              size='tiny'
+            >
+              Login!
+            </Button>
+          </>
+        )}
         <Comment.Group>
           {commentStore.comments.map((comment) => (
-          <Comment key={comment.id}>
+            <Comment key={comment.id}>
               <Comment.Avatar src={comment.image || "/assets/user.png"} />
               <Comment.Content>
-                <Comment.Author as={Link} to={`/profiles/${comment.userName}`}>{comment.displayName}</Comment.Author>
+                <Comment.Author as={Link} to={`/profiles/${comment.userName}`}>
+                  {comment.displayName}
+                </Comment.Author>
                 <Comment.Metadata>
-                  <div>{formatDistanceToNow(comment.createdAt.toISOString())} ago</div>
+                  <div>
+                    {formatDistanceToNow(comment.createdAt.toISOString())} ago
+                  </div>
                 </Comment.Metadata>
-                <Comment.Text style={{whiteSpace: 'pre-wrap'}}>{comment.body}</Comment.Text>                
+                <Comment.Text style={{ whiteSpace: "pre-wrap" }}>
+                  {comment.body}
+                </Comment.Text>
               </Comment.Content>
             </Comment>
           ))}
         </Comment.Group>
-      </Segment>      
+      </Segment>
     </>
   );
 });
